02771a4df5171438b0698b01e420997f7ad122f9
insectdie <ompusungguandry@gmail.com>
2023-08-17 13:58:33 +0700

    tipe data ts

---
+++ test/tipe-data.test.ts
@@ -0,0 +1,13 @@
+describe('Data Type',  () => {
+    it('should must declare',  () => {
+
+        const name: string = "Andry Halomoan Ompusunggu";
+        const balance: number = 100000;
+        const isVip: boolean = true;
+        
+        console.info(name);
+        console.info(balance);
+        console.info(isVip);
+
+    })
+})
